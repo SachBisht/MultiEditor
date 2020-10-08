9ff5d414a5274175284f59953fc9d795485645d8
sbisht <sbisht@cogitate.us>
2020-10-08 20:34:18 +0530

    CS files commit

---
+++ MultiEditor/Form1.cs
@@ -0,0 +1,19 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace MultiEditor
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Init Material design
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+    }
+}
